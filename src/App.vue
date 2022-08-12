049d1df89b0218b57ec47cebccb13833712babe5
Mariana Pereira <marianarp27@gmail.com>
2022-08-12 09:23:51 +0100

    fix vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,56 @@
 <template>
   <v-app>
-    <v-container>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
 
-      <splash-screen
-        v-if="!playing"
-        v-on:start-playing="startPlaying"
-      >
-      </splash-screen>
-
-      <div v-else>
-        <v-row>
-          <v-col cols="12" sm="12" md="2" class="d-flex align-center">
-              <h5>Players</h5>
-          </v-col>
-          <v-col v-for="(player, index) in players" :key="`player-${index}`">
-            <v-text-field dense label="Player" single-line v-model="players[index]"></v-text-field>
-          </v-col>
-        </v-row>
-
-        <game-round
-          v-for="(item, index) in rounds"
-          :key="'round-' + roundNames[index]"
-          :ref="'round-' + roundNames[index]"
-          :round="item"
-          :round-names="roundNames"
-          :scoreboard="scoreboard"
-          @score-update="scoreUpdate"
-        ></game-round>
-
-        <br>
-
-        <span v-if="currentRoundIndex < 7">
-          <v-btn color="light-blue darken-4 white--text"
-                 :disabled="playersCount < 2"
-                 @click="newRound">
-            New Round
-          </v-btn>
-      </span>
-        <span v-else>
-        You're on the last round.
-      </span>
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
       </div>
 
-    </v-container>
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
   </v-app>
 </template>
 
-<script lang="ts" src="./app.component.ts"></script>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App',
+
+  data: () => ({
+    //
+  })
+})
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,11 +5,12 @@ import HomeView from '../views/HomeView.vue'
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
+  // TODO create a game component and add here
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView
+  // },
   {
     path: '/about',
     name: 'about',
