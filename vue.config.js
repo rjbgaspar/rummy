2280eb96196e5e1f5bf9d30c8bf328ece8fd8d83
rjbgaspar <rjbgaspar@gmail.com>
2022-08-13 14:55:08 +0100

    Add pwa config

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,20 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    name: 'Rummy',
+    startUrl: 'index.html',
+    display: 'standalone',
+    themeColor: '#000',
+    backgroundColor: '#fff',
+    orientation: 'portrait',
+    icons: [
+      {
+        src: '"@/assets/rummy-pwa.png"',
+        sizes: '346x346',
+        type: 'image/png'
+      }
+    ]
+  }
 })
