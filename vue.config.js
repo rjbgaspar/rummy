95b62d8e6f45e644d0a0c617b4ffbe506519857e
rjbgaspar <rjbgaspar@gmail.com>
2022-08-13 15:06:44 +0100

    Add pwa imgs

--- vue.config.js
+++ vue.config.js
@@ -12,8 +12,23 @@ module.exports = defineConfig({
     orientation: 'portrait',
     icons: [
       {
-        src: '"@/assets/rummy-pwa.png"',
-        sizes: '346x346',
+        src: '@/assets/rummy-pwa-192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '@/assets/rummy-pwa-256.png',
+        sizes: '256x256',
+        type: 'image/png'
+      },
+      {
+        src: '@/assets/rummy-pwa-384.png',
+        sizes: '384x384',
+        type: 'image/png'
+      },
+      {
+        src: '@/assets/rummy-pwa-512.png',
+        sizes: '512x512',
         type: 'image/png'
       }
     ]
