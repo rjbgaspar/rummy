45bb8371dbb08613d35d7b221c0f4184fbcaf5c7
rjbgaspar <rjbgaspar@gmail.com>
2022-08-13 15:21:52 +0100

    Fix pwa img paths

--- vue.config.js
+++ vue.config.js
@@ -5,32 +5,34 @@ module.exports = defineConfig({
   ],
   pwa: {
     name: 'Rummy',
-    startUrl: 'index.html',
-    display: 'standalone',
-    themeColor: '#000',
-    backgroundColor: '#fff',
-    orientation: 'portrait',
-    icons: [
-      {
-        src: '@/assets/rummy-pwa-192.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: '@/assets/rummy-pwa-256.png',
-        sizes: '256x256',
-        type: 'image/png'
-      },
-      {
-        src: '@/assets/rummy-pwa-384.png',
-        sizes: '384x384',
-        type: 'image/png'
-      },
-      {
-        src: '@/assets/rummy-pwa-512.png',
-        sizes: '512x512',
-        type: 'image/png'
-      }
-    ]
+    manifestOptions: {
+      startUrl: 'index.html',
+      display: 'standalone',
+      themeColor: '#000',
+      backgroundColor: '#fff',
+      orientation: 'portrait',
+      icons: [
+        {
+          src: './img/icons/rummy-pwa-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/rummy-pwa-256.png',
+          sizes: '256x256',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/rummy-pwa-384.png',
+          sizes: '384x384',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/rummy-pwa-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
   }
 })
