e9a39f3d208db7e85885c90dfc765a3ddcf385e9
rjbgaspar <rjbgaspar@gmail.com>
2022-08-13 11:40:01 +0100

    Adds vuetify colors configuration

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,5 +3,15 @@ import Vuetify from 'vuetify/lib/framework'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({
-})
+const opts = {
+  theme: {
+    themes: {
+      light: {
+        primary: '#3c3f41',
+        secondary: '#b0bec5'
+      }
+    }
+  }
+}
+
+export default new Vuetify(opts)
