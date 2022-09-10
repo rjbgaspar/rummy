70b229236e31f33f97a10a67df9d9fe74ff77bda
Joao Patricio <ijpatricio@gmail.com>
2022-09-10 13:24:37 +0100

    Adds top content to an AppBar, leaving the rest with scroll. Fixed position for Action button.

--- src/components/play-rummy.vue
+++ src/components/play-rummy.vue
@@ -1,61 +1,69 @@
 <template>
-<v-container>
-  <v-row>
-    <v-col class="d-flex align-center">
-      <v-icon>mdi-account-multiple-plus</v-icon>
-    </v-col>
-    <v-col v-for="(player, index) in players" :key="`player-${index}`" >
-      <!-- Player is defined -->
-      <div>
+  <div style="position: relative">
+    <v-app-bar app :min-height="isLuckyPanelVisible ? biggerHeight : smallerHeight">
+      <v-container class="mt-2">
         <v-row>
-          <v-col class="text-right">
-            <v-btn fab x-small color="blue-grey darken-1 white--tex text-right" elevation="1"
-                   :disabled="players[index].name === '' || players[index].luckyCount === 0"
-                   @click="decrementPlayerLuckyCount(index)">
-              <v-icon dark>mdi-minus</v-icon>
-            </v-btn>
+          <v-col class="d-flex align-center" @click="isLuckyPanelVisible = !isLuckyPanelVisible">
+            <v-icon>mdi-account-multiple-plus</v-icon>
           </v-col>
-          <v-col>
-            <v-badge overlap transition="slide-x-transition"
-                     color="red"
-                     :content="players[index].luckyCount">
-              <v-text-field
-                label="playerName" single-line v-model="players[index].name"
-              >
-              </v-text-field>
-            </v-badge>
-          </v-col>
-          <v-col class="text-right">
-            <v-btn fab x-small color="blue-grey darken-1 white--tex text-right" elevation="1"
-                   :disabled="players[index].name === '' || players[index].luckyCount >= 28"
-                   @click="incrementPlayerLuckyCount(index)">
-              <v-icon dark>mdi-plus</v-icon>
-            </v-btn>
+          <v-col v-for="(player, index) in players" :key="`player-${index}`">
+            <!-- Player is defined -->
+            <div>
+              <v-row>
+                <v-col class="text-right" v-show="isLuckyPanelVisible">
+                  <v-btn fab x-small color="blue-grey darken-1 white--tex text-right" elevation="1"
+                         :disabled="players[index].name === '' || players[index].luckyCount === 0"
+                         @click="decrementPlayerLuckyCount(index)">
+                    <v-icon dark>mdi-minus</v-icon>
+                  </v-btn>
+                </v-col>
+                <v-col>
+                  <v-badge overlap transition="slide-x-transition"
+                           color="red"
+                           :content="players[index].luckyCount">
+                    <v-text-field
+                      label="playerName" single-line v-model="players[index].name"
+                    >
+                    </v-text-field>
+                  </v-badge>
+                </v-col>
+                <v-col class="text-right" v-show="isLuckyPanelVisible">
+                  <v-btn fab x-small color="blue-grey darken-1 white--tex text-right" elevation="1"
+                         :disabled="players[index].name === '' || players[index].luckyCount >= 28"
+                         @click="incrementPlayerLuckyCount(index)">
+                    <v-icon dark>mdi-plus</v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </div>
           </v-col>
         </v-row>
-      </div>
-    </v-col>
-  </v-row>
-
-  <v-divider class="my-6"></v-divider>
-
-  <game-round
-    v-for="(item, index) in rounds"
-    :key="'round-' + roundNames[index]"
-    :ref="'round-' + roundNames[index]"
-    :round="item"
-    :round-names="roundNames"
-    :scoreboard="scoreboard"
-    @score-update="scoreUpdate"
-  ></game-round>
-  <br>
-  <v-row>
-    <v-col class="d-flex justify-end">
+      </v-container>
+    </v-app-bar>
+    <v-main :style="`padding: 0; position: absolute; top: ${ isLuckyPanelVisible ? biggerHeight : smallerHeight}px;`">
+      <v-container>
+        <game-round
+          v-for="(item, index) in rounds"
+          :key="'round-' + roundNames[index]"
+          :ref="'round-' + roundNames[index]"
+          :round="item"
+          :round-names="roundNames"
+          :scoreboard="scoreboard"
+          @score-update="scoreUpdate"
+        ></game-round>
+      </v-container>
+    </v-main>
+    <div style="position: fixed; bottom: 30px; right: 20px;">
       <v-btn fab dark color="black" :disabled="playersCount < 2" @click="click">
         <v-icon>{{ actionIcon }}</v-icon>
       </v-btn>
-    </v-col>
-  </v-row>
-</v-container>
+    </div>
+  </div>
 </template>
 <script lang="ts" src="./play-rummy.component.ts"></script>
+<style>
+.v-toolbar__content {
+  height: auto !important;
+  padding: 0;
+}
+</style>
